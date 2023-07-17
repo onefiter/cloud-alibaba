09092902f0390b8a7be4e5fa8df910c0add39501
onefiter <onefiter@gmail.com>
2023-07-17 23:40:11 +0800

    nacos sample

--- cloudalibaba-consumer-nacos-order83/src/main/java/com/onefiter/OrderNacosMain83.java
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/onefiter/OrderNacosMain83.java
@@ -1,15 +1,14 @@
 package com.onefiter;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
-public class PaymentMain9001
-{
-    public static void main(String[] args) {
-        SpringApplication.run(PaymentMain9001.class, args);
+@EnableDiscoveryClient
+public class OrderNacosMain83{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(OrderNacosMain83.class,args);
     }
 }
--- cloudalibaba-provider-payment9002/src/main/java/com/onefiter/controller/PaymentController.java
+++ cloudalibaba-provider-payment9002/src/main/java/com/onefiter/controller/PaymentController.java
@@ -1,17 +1,21 @@
-package com.onefiter.springcloud.alibaba.controller;
+package com.onefiter.controller;
+
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class PaymentController {
+
     @Value("${server.port}")
     private String serverPort;
 
     @GetMapping(value = "/payment/nacos/{id}")
-    public String getPayment(@PathVariable("id") Integer id) {
-        return "serverPort: " + serverPort + " id---" + id;
+    public String getPayment(@PathVariable("id") Integer id)
+    {
+        return "serverPort: "+ serverPort+" id"+id;
     }
 }
